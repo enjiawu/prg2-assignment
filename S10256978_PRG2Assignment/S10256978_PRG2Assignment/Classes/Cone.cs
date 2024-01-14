7278cbfcc7edb462fdff8f8887129bcad33e544d
xuewenya <148406569+xuewenya@users.noreply.github.com>
2024-01-14 21:04:08 +0800

    Update Cone.cs

--- S10256978_PRG2Assignment/S10256978_PRG2Assignment/Classes/Cone.cs
+++ S10256978_PRG2Assignment/S10256978_PRG2Assignment/Classes/Cone.cs
@@ -8,5 +8,26 @@ namespace S10256978_PRG2Assignment.Classes
 {
     internal class Cone
     {
+        // Properties
+        public bool Dipped { get; set; }
+
+        // Constructors
+        public Cone() : base() { }
+        public Cone(string o, int s, List<Flavour> f, List<Topping> t, bool d) :base(o, s, f, t) 
+        {
+            Dipped = d;
+        }
+
+        // Methods
+        public override double CalculatePrice()
+        {
+            // Cone Calculation
+            return base.CalculatePrice();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
