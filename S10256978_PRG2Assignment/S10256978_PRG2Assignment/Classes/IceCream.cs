6d0fef3d245deb85f21e06ed32a2eae56c7b6734
xuewenya <148406569+xuewenya@users.noreply.github.com>
2024-01-14 20:58:44 +0800

    Update IceCream.cs

--- S10256978_PRG2Assignment/S10256978_PRG2Assignment/Classes/IceCream.cs
+++ S10256978_PRG2Assignment/S10256978_PRG2Assignment/Classes/IceCream.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace S10256978_PRG2Assignment.Classes
 {
-    internal class IceCream
+    abstract class IceCream
     {
+        //Declaring properties 
+        public string Option { get; set; }
+        public int Scoops { get; set; }
+        public List<Flavour> Flavours { get; set; } = new List<Flavour>();
+        public List<Topping> Toppings { get; set; } = new List<Topping>();
+
+
+        //Constructors
+        public IceCream() {}
+        public IceCream(string o, int s, List<Flavour> f, List<Topping> t)
+        {
+            Option = o;
+            Scoops = s;
+            Flavours = f;
+            Toppings = t;
+        }
+
+        // Methods
+        public virtual double CalculatePrice()
+        {
+            double price = 0;
+            return price;
+        }
+
+        public override string ToString()
+        {
+            return $"Option: {Option}, Scoops: {Scoops}, Flavours: {string.Join(", ", Flavours)}, Toppings: {string.Join(", ", Toppings)}";
+        }
     }
 }
