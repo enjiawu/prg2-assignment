886ad6d549caa5d3c40dea8eea55162a5370e958
xuewenya <148406569+xuewenya@users.noreply.github.com>
2024-01-14 21:00:09 +0800

    Update Cup.cs

--- S10256978_PRG2Assignment/S10256978_PRG2Assignment/Classes/Cup.cs
+++ S10256978_PRG2Assignment/S10256978_PRG2Assignment/Classes/Cup.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace S10256978_PRG2Assignment.Classes
 {
-    internal class Cup
+    internal class Cup : IceCream
     {
+        // Constructors
+        public Cup(): base() { }
+        public Cup(string o, int s,List<Flavour> f, List<Topping> t): base(o, s, f, t) { }
+
+        // Methods
+        public override double CalculatePrice()
+        {
+            // cup calculation
+            return base.CalculatePrice();
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
