a95106746e24efeb28f3301670a663729a0ba91e
xuewenya <148406569+xuewenya@users.noreply.github.com>
2024-01-16 06:28:37 +0800

    Update Flavour.cs

--- S10256978_PRG2Assignment/S10256978_PRG2Assignment/Classes/Flavour.cs
+++ S10256978_PRG2Assignment/S10256978_PRG2Assignment/Classes/Flavour.cs
@@ -8,5 +8,23 @@ namespace S10256978_PRG2Assignment.Classes
 {
     internal class Flavour
     {
+        // Declaring properties 
+        public string Type { get; set; }
+        public bool Premium { get; set; }
+        public int Quantity { get; set; }
+
+        // Constructors
+        public Flavour() { }
+        public Flavour(string t, bool p, int q) 
+        { 
+            Type = t; 
+            Premium = p;
+            Quantity = q;
+        }
+
+        public override string ToString()
+        {
+            return $"Type: {Type}\tPremium: {Premium}\tQuantity: {Quantity}";
+        }
     }
 }
