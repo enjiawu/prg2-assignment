0c6f5b26b90c1c807155f04757cb1d4cbcfd8f10
xuewenya <148406569+xuewenya@users.noreply.github.com>
2024-01-16 06:31:28 +0800

    Update Waffle.cs

--- S10256978_PRG2Assignment/S10256978_PRG2Assignment/Classes/Waffle.cs
+++ S10256978_PRG2Assignment/S10256978_PRG2Assignment/Classes/Waffle.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace S10256978_PRG2Assignment.Classes
 {
-    internal class Waffle
+    internal class Waffle : IceCream
     {
+        // Properties
+        public string WaffleFlavour { get; set; }
+
+        // Constructors
+        public Waffle() : base() { }
+        public Waffle(string o, int s, List<Flavour> f, List<Topping> t, string wf) : base(o, s, f, t)
+        {
+            WaffleFlavour = wf;
+        }
+
+        // Method
+        public override double CalculatePrice()
+        {
+            // Waffle calculation
+            double scoopPrice;
+
+            if (Scoops == 1)
+            {
+                scoopPrice = 7.00;
+            }
+            else if (Scoops == 2)
+            {
+                scoopPrice = 8.50;
+            }
+            else if (Scoops == 3)
+            {
+                scoopPrice = 9.50;
+            }
+            else
+            {
+                scoopPrice = 0.00;
+                Console.WriteLine("Warning: Invalid number of scoops. Please enter number 1 to 3.");
+            }
+
+            double price = scoopPrice + Flavours.Count * 2.00 + Toppings.Count * 1.00;
+            return price;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}\tWaffle Flavour: {WaffleFlavour}\tPrice: {CalculatePrice()}";
+        }
     }
 }
