c000444bdc903f2f4558b234f2ecda00cc872eea
xuewenya <148406569+xuewenya@users.noreply.github.com>
2024-01-16 06:27:38 +0800

    Update Topping.cs

--- S10256978_PRG2Assignment/S10256978_PRG2Assignment/Classes/Topping.cs
+++ S10256978_PRG2Assignment/S10256978_PRG2Assignment/Classes/Topping.cs
@@ -8,5 +8,19 @@ namespace S10256978_PRG2Assignment.Classes
 {
     internal class Topping
     {
+        // Declaring properties 
+        public string Type {  get; set; }
+
+        // Constructors
+        public Topping() { }
+        public Topping(string t) 
+        {
+            Type = t; 
+        }
+
+        public override string ToString()
+        {
+            return $"Type: {Type}";
+        }
     }
 }
